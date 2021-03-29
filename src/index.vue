6b1ccc9d3e049f3521eff732b05235379ab1a3af
李响 <lixzap@gmail.com>
2021-03-29 18:22:13 +0800

    init

---
+++ src/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <router-view/>
+    <div class="tabbar">
+      <div class="tabbar-item">药丸</div>
+      <div class="tabbar-item">我的</div>
+      <div class="tabbar-item"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png',
+    };
+  },
+};
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+  .logo {
+    width: 424px;
+    height: 200px;
+  }
+  .greeting {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
